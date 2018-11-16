24e943c8ebc2220800d81fa16732c1a3766418aa
Ethan <weiqiang1994.cwq@gmail.com>
2018-11-16 17:41:39 +0800

    Revert "meal tab done"
    This reverts commit 36987a380518cb7f010dc7d37ce7d07173398ea1.

--- hci-project/screens/MealScreen.js
+++ hci-project/screens/MealScreen.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import { StyleSheet, Image, Dimensions, TouchableHighlight } from 'react-native';
-import { Container, Content, Left, Body, Text, CardItem, Card, Thumbnail, View } from "native-base";
+import { ScrollView, StyleSheet ,Alert} from 'react-native';
+import { ExpoLinksView } from '@expo/samples';
+import { Container, Content, Left, Body, Text, CardItem, Card, Thumbnail } from "native-base";
+import { createStackNavigator } from 'react-navigation';
+import AppNavigator from "../navigation/AppNavigator"
 
-const { width } = Dimensions.get('window');
-const height = width * 0.8
 
 export default class MealScreen extends React.Component {
   static navigationOptions = {
-    title: 'Meals',
+    title: 'Meal',
   };
-
+  
   render() {
-
     return (
       <Container>
         <Content padder>
-          <TouchableHighlight onPress={() => this.props.navigation.navigate('Meal1')}>
-            <Card style={{ flex: 0 }}>
-              <View style={styles.container}>
-                <Image style={{ flex: 1, width: '100%', height: 200 }} source={require('../images/meals/Smoked_Brisket_with_Zesty_Barbecue_Sauce.jpg')} />
-              </View>
-              <CardItem>
+          <Card style={{ flex: 0 }}>
+            <CardItem button onPress={()=>this.props.navigation.navigate('Meal1')}>
+              <Left>
+                <Thumbnail square source={require('../images/meals/Smoked_Brisket_with_Zesty_Barbecue_Sauce.jpg')} />
                 <Body>
                   <Text>Smoked Brisket with Zesty Barbecue Sauce</Text>
                   <Text note>Summary: Tasty Chicken rice with curry</Text>
@@ -30,17 +28,13 @@ export default class MealScreen extends React.Component {
                   <Text note>Fats: 200</Text>
                   <Text note>Sodium: 200</Text>
                 </Body>
-              </CardItem>
-            </Card>
-          </TouchableHighlight>
-
-          <TouchableHighlight onPress={() => this.props.navigation.navigate('Meal2')}>
-            <Card style={{ flex: 0 }}>
-
-              <View style={styles.container}>
-                <Image style={{ flex: 1, width: '100%', height: 200 }} source={require('../images/meals/carrot_soup.jpg')} />
-              </View>
-              <CardItem>
+              </Left>
+            </CardItem>
+          </Card>
+          <Card style={{ flex: 0 }}>
+            <CardItem button onPress={()=>this.props.navigation.navigate('Meal2')}>
+              <Left>
+                <Thumbnail square source={require('../images/meals/carrot_soup.jpg')} />
                 <Body>
                   <Text>Carrot Soup</Text>
                   <Text note>Summary: Tasty Chicken rice with curry</Text>
@@ -50,69 +44,59 @@ export default class MealScreen extends React.Component {
                   <Text note>Fats: 200</Text>
                   <Text note>Sodium: 200</Text>
                 </Body>
-              </CardItem>
-            </Card>
-          </TouchableHighlight>
-
-        <TouchableHighlight onPress={() => this.props.navigation.navigate('Meal2')}>
-          <Card style={{ flex: 0 }}>
-            <View style={styles.container}>
-              <Image style={{ flex: 1, width: '100%', height: 200 }} source={require('../images/meals/chocolate_banana_protein_smoothie.jpg')} />
-            </View>
-            <CardItem>
-              <Body>
-                <Text>Chocolate Banana Protein Smoothie</Text>
-                <Text note>Summary: Tasty Chicken rice with curry</Text>
-                <Text note>Calories: 190kcal</Text>
-                <Text note>Protein: 200</Text>
-                <Text note>Carbs: </Text>
-                <Text note>Fats: 200</Text>
-                <Text note>Sodium: 200</Text>
-              </Body>
+              </Left>
             </CardItem>
           </Card>
-          </TouchableHighlight>
-
-          
-        <TouchableHighlight onPress={() => this.props.navigation.navigate('Meal2')}>
           <Card style={{ flex: 0 }}>
-            <View style={styles.container}>
-              <Image style={{ flex: 1, width: '100%', height: 200 }} source={require('../images/meals/Roast_with_Spicy_Potatoes.jpg')} />
-            </View>
             <CardItem>
-              <Body>
-                <Text>Roast with Spicy Potatoes</Text>
-                <Text note>Summary: Tasty Chicken rice with curry</Text>
-                <Text note>Calories: 190kcal</Text>
-                <Text note>Protein: 200</Text>
-                <Text note>Carbs: </Text>
-                <Text note>Fats: 200</Text>
-                <Text note>Sodium: 200</Text>
-              </Body>
+              <Left>
+                <Thumbnail square source={require('../images/meals/chocolate_banana_protein_smoothie.jpg')} />
+                <Body>
+                  <Text>Chocolate Banana Protein Smoothie</Text>
+                  <Text note>Summary: Tasty Chicken rice with curry</Text>
+                  <Text note>Calories: 190kcal</Text>
+                  <Text note>Protein: 200</Text>
+                  <Text note>Carbs: </Text>
+                  <Text note>Fats: 200</Text>
+                  <Text note>Sodium: 200</Text>
+                </Body>
+              </Left>
             </CardItem>
           </Card>
-          </TouchableHighlight>
-
-          <TouchableHighlight onPress={() => this.props.navigation.navigate('Meal2')}>
           <Card style={{ flex: 0 }}>
-            <View style={styles.container}>
-              <Image style={{ flex: 1, width: '100%', height: 200 }} source={require('../images/meals/Black_Bean_Chili.jpg')} />
-            </View>
             <CardItem>
-            <Body>
-              <Text>Black Bean Chili</Text>
-              <Text note>Summary: Tasty Chicken rice with curry</Text>
-              <Text note>Calories: 190kcal</Text>
-              <Text note>Protein: 200</Text>
-              <Text note>Carbs: </Text>
-              <Text note>Fats: 200</Text>
-              <Text note>Sodium: 200</Text>
-            </Body>
+              <Left>
+                <Thumbnail square source={require('../images/meals/Roast_with_Spicy_Potatoes.jpg')} />
+                <Body>
+                  <Text>Roast with Spicy Potatoes</Text>
+                  <Text note>Summary: Tasty Chicken rice with curry</Text>
+                  <Text note>Calories: 190kcal</Text>
+                  <Text note>Protein: 200</Text>
+                  <Text note>Carbs: </Text>
+                  <Text note>Fats: 200</Text>
+                  <Text note>Sodium: 200</Text>
+                </Body>
+              </Left>
             </CardItem>
           </Card>
-          </TouchableHighlight>
+          <Card style={{ flex: 0 }}>
+            <CardItem>
+              <Left>
+                <Thumbnail square source={require('../images/meals/Black_Bean_Chili.jpg')} />
+                <Body>
+                  <Text>Black Bean Chili</Text>
+                  <Text note>Summary: Tasty Chicken rice with curry</Text>
+                  <Text note>Calories: 190kcal</Text>
+                  <Text note>Protein: 200</Text>
+                  <Text note>Carbs: </Text>
+                  <Text note>Fats: 200</Text>
+                  <Text note>Sodium: 200</Text>
+                </Body>
+              </Left>
+            </CardItem>
+          </Card>
         </Content>
-      </Container >
+      </Container>
     );
   }
 }
@@ -120,12 +104,7 @@ export default class MealScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-
-  },
-  image: {
-    width,
-    height,
+    paddingTop: 15,
+    backgroundColor: '#fff',
   },
 });
